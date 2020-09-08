62c771833e66d9d0a7dcfd41b905c753a4334601
Edward Leung <edward_leung_12@hotmail.com>
2020-09-08 23:16:07 +0100

    added a todo function

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 // Event Listeners
+todoButton.addEventListener("click", addTodo);
 
 // Functions
+function addTodo(evt) {
+  // Prevent form from submitting
+  evt.preventDefault();
+}
