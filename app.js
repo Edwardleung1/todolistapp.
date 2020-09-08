9f00c0a08030221348d4884b8041f126364a485a
Edward Leung <edward_leung_12@hotmail.com>
2020-09-08 23:40:06 +0100

    created a todo DIV when the submit is clicked

--- app.js
+++ app.js
@@ -10,4 +10,35 @@ todoButton.addEventListener("click", addTodo);
 function addTodo(evt) {
   // Prevent form from submitting
   evt.preventDefault();
+
+  // Create todo DIV
+  const todoDiv = document.createElement("div");
+  // Add a class to it
+  todoDiv.classList.add("todo");
+  // Create LI
+  const newTodo = document.createElement("li");
+  // Add a value to it
+  newTodo.innerText = "hello";
+  // Add a class to it
+  newTodo.classList.add("todo-item");
+  // Append child to the todo DIV
+  todoDiv.appendChild("newTodo");
+
+  // Create check BUTTON
+  const completedButton = document.createElement("button");
+  // Add i tag inside Button
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  // Add a class to it
+  completedButton.classList.add("complete-btn");
+  // Append child to the todo DIV
+  todoDiv.appendChild("completedButton");
+
+  // Create trash BUTTON
+  const trashButton = document.createElement("button");
+  // Add i tag inside Button
+  trashButton.innerHTML = '<i class="fas fa-trash"></li>';
+  // Add a class to it
+  trashButton.classList.add("complete-btn");
+  // Append child to the todo DIV
+  todoDiv.appendChild("trashButton");
 }
